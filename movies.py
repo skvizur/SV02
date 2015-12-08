7919b09a9646654dce9c7c8de5603e78a682c0c1
Fanndis Kristinsdottir <Fanndis@orvar.hir.is>
2015-12-08 15:51:58 +0000

    thrjar toflur komnar

--- movies.py
+++ movies.py
@@ -1,13 +1,14 @@
 import pandas as pd
 
-f = open('movies.dat', 'r')
+cols_movies = ['MovieID','Title', 'Genres']
+cols_rating = ['UserID', 'MovieID', 'Rating','TimeStamp']
+cols_user = ['UserID', 'Gender', 'Age', 'Occupation','Zip Code']
 
-cols = ['MovieID','Title', 'Genres']
+movies_table = pd.read_table(r'movies.dat', sep='::', header = None, names = cols_movies)
+rating_table = pd.read_table(r'ratings.dat', sep='::', header = None, names = cols_rating)
+user_table = pd.read_table(r'users.dat', sep='::',engine='python', header = None, names = cols_user)
 
-movies_table = pd.read_table(r'movies.dat', sep='::', header = None, names = cols)
-
-print(movies_table)
+print(user_table)
 
 
-
-f.close()
+#HVERNIG LOKA EG SKRÁNNI??
