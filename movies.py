dc6f6467b0bfb7940225301f6b6e4c0f63cbf1c0
Fanndis Kristinsdottir <Fanndis@orvar.hir.is>
2015-12-08 21:03:20 +0000

    breytingar sem virka ekki

--- movies.py
+++ movies.py
@@ -1,14 +1,31 @@
 import pandas as pd
+import string
 
-cols_movies = ['MovieID','Title', 'Genres']
-cols_rating = ['UserID', 'MovieID', 'Rating','TimeStamp']
-cols_user = ['UserID', 'Gender', 'Age', 'Occupation','Zip Code']
+#cols_movies = ['MovieID','Title', 'Genres']
+#cols_rating = ['UserID', 'MovieID', 'Rating','TimeStamp']
+#cols_user = ['UserID', 'Gender', 'Age', 'Occupation','Zip Code']
 
-movies_table = pd.read_table(r'movies.dat', sep='::', header = None, names = cols_movies)
-rating_table = pd.read_table(r'ratings.dat', sep='::', header = None, names = cols_rating)
-user_table = pd.read_table(r'users.dat', sep='::',engine='python', header = None, names = cols_user)
+#movies_table = pd.read_table(r'movies.dat', sep='::', header = None, names = cols_movies)
+#rating_table = pd.read_table(r'ratings.dat', sep='::', header = None, names = cols_rating)
+#user_table = pd.read_table(r'users.dat', sep='::', header = None, names = cols_user)
 
-print(user_table)
+superheroes = {}
+
+index = 0
+
+with open('movies.dat') as infile:
+	for line in infile:
+		parts = line.split('::')
+		if len(line) > 1:
+			superheroes[index] = {'MovieID': parts[0],'Title':parts[1],'Genres':parts[2]}
+			index +=1
 
 
-#HVERNIG LOKA EG SKRÁNNI??
+outfile = open('movies_demo.sql','w')
+
+for s in superheroes:
+	outfile.write("insert into movies (MovieID,Title,Genres) values('{}','{}','{}')\n".format(s.replace("'","'"),superheroes[s]['MovieID'],superheroes[s]['Title'])
+
+outfile.close()
+
+		
