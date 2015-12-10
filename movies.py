6c84a69b04cd9ef22943c0cb5ce6a624b5b9b6e5
Fanndis Kristinsdottir <Fanndis@lenni.hir.is>
2015-12-10 13:22:35 +0000

    fjorar toflur, virka allar

--- movies.py
+++ movies.py
@@ -1,11 +1,12 @@
 import pandas as pd
 import string
+import itertools
 
 cols_movies = ['MovieID','Title', 'Genres']
 #cols_rating = ['UserID', 'MovieID', 'Rating','TimeStamp']
 #cols_user = ['UserID', 'Gender', 'Age', 'Occupation','Zip Code']
 
-movies_table_panda = pd.read_csv('movies.dat', sep='::', header = None, names = cols_movies)
+movies_table_panda = pd.read_csv('movies.dat', sep='::', header = None, names = cols_movies, engine='python')
 #rating_table = pd.read_table(r'ratings.dat', sep='::', header = None, names = cols_rating)
 #user_table = pd.read_table(r'users.dat', sep='::', header = None, names = cols_user)
 
@@ -20,39 +21,81 @@ for line in only_last:
 		new_list.append(i)
 
 new_list = sorted(set(new_list))
+number_list = list(range(0,19))
 
-genres_table = {}
-index=1
+
+genres_id_table = {}
+i=1
 
 for word in new_list:
-	genres_table[index] = {'id': index, 'genres': word}
-	print(genres_table[index])
-	index += 1
+	genres_id_table[word] =i
+	print(genres_id_table[word])
+	i += 1
 
-movies_table = {}
 
-index = 0
+adict = dict(zip(new_list,number_list))
+print(adict)
+
+s = movies_table_panda['Genres'].str.split('|').apply(pd.Series,1).stack()
+s.index = s.index.droplevel(-1)
+s.name = 'Genres'
+
+del movies_table_panda['Genres']
+y = movies_table_panda.join(s)
+
+movie_id_col = list(y.iloc[:,0])
+movie_genre_col = list(y.iloc[:,2])
+
+movieid_genresid = {}
+for i in range(len(movie_id_col)):
+	movieid_genresid[i] = {'movies_id': movie_id_col[i], 'genreid': genres_id_table[movie_genre_col[i]]}
+
+
+
+#print(movieId_genre_table)
+#print(movieId_genre_table)
+#index = 0
+
+#print(range(len(movie_id_col)))
+
+#for item in movie_id_col:
+#	movieId_genre_table[index] = {'MovieID':item,'Genres':movie_genre_col[index]}
+#	print(movieId_genre_table[index])
+#	index +=1
+
+
+#area_dict = dict(zip(y['MovieID'], y['Genres']))
+
 
 f = open('movies.dat','r')
 
-for line in f:
-	parts = line.split('::')
-	if len(line) > 1:
-		movies_table[index] = {'movieid': parts[0],'title':parts[1],'genres':parts[2]}
-		index +=1
+#GRUNNUR AD MOVIE TÖFLU MED ID TITLI OG GENRES (ÞARF ANNAÐ HVORT ÞESSA EÐA HINA FYRIR NEÐAN)
+#movies_table = {}
+index = 0
+
+#for line in f:
+#	parts = line.split('::')
+#	if len(line) > 1:
+#		movies_table[index] = {'movieid': parts[0],'title':parts[1],'genres':parts[2]}
+#		index +=1
 
 
+#GRUNNUR AD MOVIE TÖFLU MED ID, TITLI OG ÁRI.
 movie_table = {}
 
 for line in f:
 	parts1 = line.split('::')
 	if len(line) > 1:
-		title = parts1[1].split('(')
+		title = parts1[1].rsplit('(',1)
 		updated_title=title[1].strip(')')
-		movie_table[0] = {'id': parts1[0], 'title':title[0],'year':updated_title}
+		movie_table[index] = {'movieid': parts1[0], 'title':title[0],'year':updated_title}
+		print(movie_table[index])
+		index +=1
 
 
 f.close()
+
+#TILLAGA AD TOFLU MED OLLUM TYPUM GENRES
 
 #genre_table = {}
 #result = {}
@@ -71,45 +114,52 @@ f.close()
 #				if value not in result.values():
 #					result[key] = value
 #					print(result[key])
-			
-					
-index1 = 0
+	
 
+
+
+#GRUNNUR AD RATING TÖFLLU					
+index = 0
 rating_table = {}
 
 with open('ratings.dat') as infile:
 	for line in infile:
 		parts = line.split('::')
 		if len(line) > 1:
-			rating_table[index1] = {'user_id': parts[0],'movie_id':parts[1],'rating':parts[2],'timestamp':parts[3]}
-			index1 +=1
+			rating_table[index] = {'user_id': parts[0],'movie_id':parts[1],'rating':parts[2]}
+			index +=1
 
 
-
-index2 = 0
-
+#GRUNNUR AÐ NOTENDATÖFLU
+index = 0
 user_table = {}
 
 with open('users.dat') as infile:
 	for line in infile:
 		parts = line.split('::')
 		if len(line) > 1:
-			user_table[index2] = {'userid': parts[0],'gender': parts[1],'age': parts[2],'occupation': parts[3],'zipcode': parts[4]}
-			index2 +=1
+			user_table[index] = {'userid': parts[0],'gender': parts[1],'age': parts[2],'occupation': parts[3]}
+			index +=1
 
 
 
 outfile = open('movies_demo.sql','w')
 
-for s in movies_table:
-	outfile.write("insert into movies (movieid, title, genres) values('{}','{}','{}')\n".format(movies_table[s]['movieid'],movies_table[s]['title'].replace("'","''"),movies_table[s]['genres'].replace("'","''").strip()))
+for s in movie_table:
+	outfile.write("insert into movies (movieid, title, year) values('{}','{}','{}')\n".format(movie_table[s]['movieid'],movie_table[s]['title'].replace("'","''"),movie_table[s]['year'].strip()))
 
 for a in user_table:
-	outfile.write("insert into username (userid, gender, age, occupation, zipcode) values('{}','{}','{}','{}','{}')\n".format(user_table[a]['userid'],user_table[a]['gender'],user_table[a]['age'],user_table[a]['occupation'],user_table[a]['zipcode'].strip()))
+	outfile.write("insert into username (userid, gender, age, occupation) values('{}','{}','{}','{}')\n".format(user_table[a]['userid'],user_table[a]['gender'],user_table[a]['age'],user_table[a]['occupation'].strip()))
 
 for b in rating_table:
-	outfile.write("insert into rating (user_id, movie_id, rating, timestamp) values('{}','{}','{}','{}')\n".format(rating_table[b]['user_id'],rating_table[b]['movie_id'],rating_table[b]['rating'],rating_table[b]['timestamp'].strip()))
+	outfile.write("insert into rating (user_id, movie_id, rating) values('{}','{}','{}')\n".format(rating_table[b]['user_id'],rating_table[b]['movie_id'],rating_table[b]['rating'].strip()))
+
+for k in movieid_genresid:
+	outfile.write("insert into genre_id (movies_id, genreid) values('{}','{}')\n".format(movieid_genresid[k]['movies_id'],movieid_genresid[k]['genreid']))
 
 outfile.close()
+
+
+#ATH MUNA AÐ SETJA INDEXA
 
 		
